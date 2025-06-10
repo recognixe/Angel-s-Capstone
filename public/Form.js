47d44da0b19fac1418a61145dad1933caa271354
Mkrah <angel.felix25@compscihigh.org>
2025-06-10 18:37:39 +0000

    completed

--- public/Form.js
+++ public/Form.js
@@ -5,12 +5,40 @@ createForm.addEventListener('submit', async (e)=>{
 	const bookData = new FormData(createForm)
 	const reqBody = Object.fromEntries(bookData)
 
-	const response = await fetch('/add', {
+	const response = await fetch('/add/pets.', {
 		method: 'POST',
 		headers: {
-			'Content-Type': 'application/json')
+			'Content-Type': 'application/json'
 		},
 		body: JSON.stringify(reqBody)
 	})
     window.location.href = '/'
 })
+
+async function deletePet(id) {
+ await fetch('/delete/' + id, {method: 'DELETE'});
+ window.location.href = "/pets"
+}
+
+async function editPet(e, id) {
+ e.preventDefault();
+
+ const formData = new FormData(e.target);
+ const formObject = Object.fromEntries(formData.entries());
+
+ await fetch('/Pets/' + id, {
+   method: 'PATCH',
+   headers: { 'Content-Type': 'application/json' },
+   body: JSON.stringify(formObject)
+ });
+
+
+
+
+ window.location.href = '/'
+}
+
+async function deletePet(id) {
+ await fetch('/deletepets/' + id, {method: 'DELETE'});
+ window.location.href = "/"
+}
